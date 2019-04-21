fa525a52994dd2398de3221925a582f7ca1f35b6
tiskander <tiskander1@outlook.com>
2019-04-20 20:36:42 -0400

    sd

---
+++ Javascript/Exercise_1/script.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("Hi, whats you first name?");
+var userLastName = prompt("What's your last name?");
+var userAge = prompt("What's your age?");
+console.log("Your full name is " + userFirstName + " " + userLastName );
+console.log("Your are " + userAge + " old.")
