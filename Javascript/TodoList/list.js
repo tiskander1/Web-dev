1521d665619285a70c9b8d3ac58f1063f33a96ab
tiskander1@outlook.com <tiskander1@outlook.com>
2019-05-24 18:25:09 -0400

    ss

---
+++ Javascript/TodoList/list.js
@@ -0,0 +1,20 @@
+console.log("connected");
+
+var todos = [];
+
+var input = prompt ("WHat would you like to do?")
+
+
+while(input != "quit"){
+    if (input === "list") {
+        console.log(todos);
+    } else if (input === "new"){
+        var newtodo = prompt("Enter new todo!")
+        //add to new new to do 
+        todos.push(newtodo);
+    }
+    //ask again fo rnew input 
+    input = prompt("What would you like to do?");
+}
+
+console.log("OK, YOU QUIT THE APP!!!");
